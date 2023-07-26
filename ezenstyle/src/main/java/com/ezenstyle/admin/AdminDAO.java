8acab168c445c58b90ee68395009bd0779d00518
DESKTOP-C1T83CR\USER <ajy4606@naver.com>
2023-07-26 17:29:05 +0900

    관리자 패키지

---
+++ ezenstyle/src/main/java/com/ezenstyle/admin/AdminDAO.java
@@ -0,0 +1,17 @@
+package com.ezenstyle.admin;
+
+import java.sql.*;
+import java.util.*;
+
+public class AdminDAO {
+	
+	private Connection conn;
+	private PreparedStatement ps;
+	private ResultSet rs;
+	
+	public AdminDAO() {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
