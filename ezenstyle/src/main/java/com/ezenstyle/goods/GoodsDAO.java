bcd8233e177c25a81286b7f17d3123ccc293fe54
You-Seongjin <seongjin511@naver.com>
2023-07-28 09:45:47 +0900

    품절 상품 리스트 기능 _ 유성진

--- ezenstyle/src/main/java/com/ezenstyle/goods/GoodsDAO.java
+++ ezenstyle/src/main/java/com/ezenstyle/goods/GoodsDAO.java
@@ -403,5 +403,46 @@ public class GoodsDAO {
 		
 	}
 	
+	public ArrayList<GoodsDTO> soidoutlist(){
+		try {
+			String sql="select * from semi_goods where g_stock=0 order by category desc";
+			ps=conn.prepareStatement(sql);
+			rs=ps.executeQuery();
+			ArrayList<GoodsDTO> arr=new ArrayList<GoodsDTO>();
+			
+			while (rs.next()) {
+				int idx=rs.getInt("idx");
+				String category=rs.getString("g_category");
+				String name=rs.getString("g_name");
+				String ofile=rs.getString("g_ofile");
+				String nfile=rs.getString("g_nfile");
+				String color=rs.getString("g_color");
+				String size=rs.getString("g_size");
+				int stock=rs.getInt("g_stock");
+				String detail=rs.getString("g_detail");
+				int readnum=rs.getInt("readnum");
+				
+				GoodsDTO dto=new GoodsDTO(idx, name, ofile, nfile, color, size, stock, stock, category, detail, readnum);
+				
+				arr.add(dto);	
+			}
+			return arr;
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}finally {
+			try {
+				if(rs!=null)rs.close();
+				if(ps!=null)ps.close();
+				if(conn!=null)conn.close();
+			}catch(Exception e1) {
+				e1.printStackTrace();
+			}
+		}
+		
+		
+	}
+	
 	
 }
